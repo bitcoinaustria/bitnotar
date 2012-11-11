cc1f7675ea30f0db1b951ea766dcb34b485aa6db
Andreas Petersson <andreas@petersson.at>
2012-11-11 12:35:20 +0100

    notary stub 2

--- androidBitnotar/src/at/bitcoinaustria/bitnotar/Notary.java
+++ androidBitnotar/src/at/bitcoinaustria/bitnotar/Notary.java
@@ -1,15 +1,22 @@
 package at.bitcoinaustria.bitnotar;
 
+import android.content.ContentResolver;
 import android.os.AsyncTask;
 import com.google.bitcoin.core.Sha256Hash;
+import com.google.common.base.Preconditions;
+import com.google.common.hash.HashFunction;
+import com.google.common.hash.Hashing;
+import com.google.common.io.ByteStreams;
+import com.google.common.io.InputSupplier;
 
 import java.io.*;
 import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 /**
  * @author apetersson
  */
-public class Notary extends AsyncTask<InputStream,Float,Sha256Hash> {
+public class Notary extends AsyncTask<ContentResolver,Long,Sha256Hash> {
 
 /*
 
@@ -33,10 +40,12 @@ public class Notary extends AsyncTask<InputStream,Float,Sha256Hash> {
     }
 
     @Override
-    protected Sha256Hash doInBackground(InputStream... params) {
+    protected Sha256Hash doInBackground(ContentResolver... params) {
+
         return Sha256Hash.ZERO_HASH;
-      /*  publishProgress();
-        int buff = 16384;
+/*        int buff = 16384;
+        Preconditions.checkPositionIndex(1,params.length);
+        final ContentResolver stream = params[0];
         try {
            // RandomAccessFile file = new RandomAccessFile("T:\\someLargeFile.m2v", "r");
 
@@ -49,6 +58,16 @@ public class Notary extends AsyncTask<InputStream,Float,Sha256Hash> {
             long read = 0;
 
             // calculate the hash of the hole file for the test
+
+            ByteStreams.hash(new InputSupplier<InputStream>() {
+                @Override
+                public InputStream getInput() throws IOException {
+                    return stream.;
+                }
+            }, Hashing.sha256());
+
+            int readBytes; readBytes = stream.read(buffer);
+            hashSum.update(buffer);
             long offset = file.length();
             int unitsize;
             while (read < offset) {
@@ -66,8 +85,14 @@ public class Notary extends AsyncTask<InputStream,Float,Sha256Hash> {
 
             long endTime = System.nanoTime();
 
+
             System.out.println(endTime - startTime);
 
-        }*/
-}
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+*/
+    }
 }
